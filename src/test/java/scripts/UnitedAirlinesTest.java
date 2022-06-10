580de32fde69947aa34a9f305d7643f00181f757
ReginaBik <reginabiktimirova1@gmail.com>
2022-06-10 17:02:39 -0500

    touch up

--- src/test/java/scripts/UnitedAirlinesTest.java
+++ src/test/java/scripts/UnitedAirlinesTest.java
@@ -23,4 +23,44 @@ public class UnitedAirlinesTest extends Base{
     }
 
 
+    /*Test Case 2: Validate "Book travel menu" navigation items
+    Given user is on "https://www.united.com/en/us"
+    Then user should see "Book travel menu" navigation items
+    |Book             |
+    |Flight Status|
+    |Check-in       |
+    |My trips        |*/
+
+    /*Test Case 3: Validate "Round-trip" and "One-way" radio buttons
+    Given user is on "https://www.united.com/en/us"
+    Then validate "Roundtrip" radio button is displayed, is enabled and is selected
+    And validate "One-way" radio button is displayed, is enabled but is not selected
+    When user clicks on "One-way" radio button
+    Then validate "One-way" radio button is selected while "Roundtrip" radio button is
+    deselected*/
+
+    /*Test Case 4: Validate "Book with miles" and "Flexible dates" checkboxes
+    Given user is on "https://www.united.com/en/us"
+    Then validate "Book with miles" checkbox is displayed, is enabled but is not selected
+    And validate "Flexible dates" checkbox is displayed, is enabled but is not selected
+    When user clicks both checkboxes
+    Then validate both checkboxes are selected
+    When user clicks on both selected checkboxes again
+    Then validate both checkboxes are deselected*/
+
+    /*Test Case 5: Validate One-way ticket search results "from Chicago, IL, US (ORD) to
+    Miami, FL, US (MIA)”
+    Given user is on "https://www.united.com/en/us"
+    When user selects "One-way" ticket radio button
+    And user enters "Chicago, IL, US (ORD)" to from input box
+    And user enters "Miami, FL, US (MIA)" to to input box
+    And user selects "Jan 30" to the dates input box
+    And user selects "2 Adults" from travelers selector
+    And user selects "Business or First" from cabin dropdown
+    And user clicks on "Find Flights" button
+    Then validate departure equals to "Depart: Chicago, IL, US to Miami, FL, US*/
+
+
+
+
 }
